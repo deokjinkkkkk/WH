c8cdd1a4a102aa13aabeb505a7757d25db0b9bad
gkals0230@naver.com <gkals0230@naver.com>
2023-02-21 16:22:38 +0900

    notice

---
+++ whProject/src/main/java/co/admin/wh/notice/NoticeController.java
@@ -0,0 +1,16 @@
+package co.admin.wh.notice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import co.admin.wh.notice.mapper.NoticeMapper;
+@Controller
+public class NoticeController {
+	@Autowired NoticeMapper noticeMapper;
+	
+	@RequestMapping("/notice")
+	public String noticeForm() {
+		return "notice/noticelist";
+	}
+}
---
+++ whProject/src/main/java/co/admin/wh/notice/mapper/NoticeMapper.java
@@ -0,0 +1,18 @@
+package co.admin.wh.notice.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import co.admin.wh.notice.NoticeVO;
+
+public interface NoticeMapper {
+
+	List<NoticeVO> noticeList();
+	NoticeVO noticeSelect(NoticeVO vo);
+	int noticeInsert(NoticeVO vo);
+	int noticeUpdate(NoticeVO vo);
+	int noticeDelete(NoticeVO vo);
+	
+	List<NoticeVO> noticeSearch(@Param("key") String key, @Param("val") String val);
+}
