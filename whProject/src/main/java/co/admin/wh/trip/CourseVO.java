ea080fb12f51bbec324c76ca77e585eedb09a1e3
seonhee <ads707@naver.com>
2023-02-21 17:35:00 +0900

    2023_02_21 여행,코스VO + ...

---
+++ whProject/src/main/java/co/admin/wh/trip/CourseVO.java
@@ -0,0 +1,23 @@
+package co.admin.wh.trip;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class CourseVO {
+	// 코스
+	String couCode; //코스번호
+	int tripCode; //여행자번호
+	String couName; //코스명
+	String couDist; //코스길이
+	String sido; //행정구역
+	int time; //총소요시간
+	String couContent; //코스설명
+	Date couRegDate; //코스등록일자
+	Date couModDate; //코스수정일자
+	String couLat; //위도
+	String couLon; //경도
+	String imgGroCode; //이미지그룹번호
+	
+}
--- whProject/src/main/java/co/admin/wh/trip/TripVO.java
+++ whProject/src/main/java/co/admin/wh/trip/TripVO.java
@@ -5,17 +5,18 @@ import java.sql.Date;
 import lombok.Data;
 
 @Data
-public class TripVO {
-	int tripCode;
-	String tripName;
-	String tripTel;
-	String tripAddr;
-	String tripLat;
-	String tripLon;
-	String TripSumm;
-	String TripContent;
-	String homepage;
-	String tripRegion;
-	String imgGroCode;
-	Date tripDate;
+public class TripVO { 
+	// 여행지
+	int tripCode; //여행번호
+	String tripName; //명칭
+	String tripTel; //연락처
+	String tripAddr; //주소
+	String tripLat; //위도
+	String tripLon; //경도
+	String TripSumm; //개요
+	String TripContent; //상세정보
+	String homepage; //홈페이지
+	String tripRegion; //지역
+	String imgGroCode; //이미지그룹번호
+	Date tripDate; //등록일자
 }
