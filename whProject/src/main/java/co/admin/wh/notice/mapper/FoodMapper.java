2caa4dcb3cb45920132e8aab31d47f245418eb07
gim-eunjin <gim-eunjin@localhost>
2023-02-22 17:54:22 +0900

    23.02/22

--- whProject/src/main/java/co/admin/wh/notice/mapper/FoodMapper.java
+++ whProject/src/main/java/co/admin/wh/notice/mapper/FoodMapper.java
@@ -2,17 +2,10 @@ package co.admin.wh.notice.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Param;
-
 import co.admin.wh.notice.vo.FoodVO;
 
 public interface FoodMapper {
-	
-	List<FoodVO> foodList();
-	FoodVO foodSelect(FoodVO vo);
-	int foodInsert(FoodVO vo);
-	int foodUpdate(FoodVO vo);
-	int foodDelete(FoodVO vo);
-	
-	List<FoodVO> foodSearch(@Param("key") String key, @Param("val") String val);
+
+	List<FoodVO> getFoodList();
+	FoodVO getFoodSelect(FoodVO vo);
 }
--- whProject/src/main/java/co/admin/wh/notice/vo/FoodVO.java
+++ whProject/src/main/java/co/admin/wh/notice/vo/FoodVO.java
@@ -1,23 +1,23 @@
 package co.admin.wh.notice.vo;
 
-import org.springframework.format.annotation.DateTimeFormat;
+import java.sql.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-
 import lombok.Data;
 
 @Data
 public class FoodVO {
-	int foodCode;
-	String id;
-	String foodTitle;
-	String foodName;
-	String foodAddr;
-	String foodContent;
-	int foodHit;
-	String foodFile;
-	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
-	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	String foodDate;
+	private int foodCode; //맛집 번호
+	private String id;	//회원 아이디
+	private String foodTitle;	//제목
+	private String foodName;	//상호명
+	private String foodAddr;	//주소	
+	private String foodContent;	//내용
+	private int foodHit;	//조회수	
+	private String foodFile;	//첨부파일
+	@JsonFormat(pattern="yyyy-MM-dd")	
+	private Date foodDate;	//등록일자
+	private String imgGroCode;	//이미지그룹번호
+	
 	
 }
