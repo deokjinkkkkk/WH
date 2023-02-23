ac6b060ba101ae592c7c9b8c70993cea49d51f4e
deokjinkkkkk <antan3957@naver.com>
2023-02-23 14:18:06 +0900

    2023/02/23

--- whProject/src/main/java/co/admin/wh/common/vo/ComentsVO.java
+++ whProject/src/main/java/co/admin/wh/common/vo/ComentsVO.java
@@ -1,5 +1,21 @@
 package co.admin.wh.common.vo;
 
-public class ComentsVO {
+import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Data;
+
+@Data
+public class ComentsVO {
+	int comCode;
+	String id;
+	String comContent;
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	Date comDate;
+	int comLock;
+	String comOrder;
+	String comGroup;
+	String block;
+	int comFlag;
 }
--- whProject/src/main/java/co/admin/wh/common/web/ComentsController.java
+++ whProject/src/main/java/co/admin/wh/common/web/ComentsController.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ComentsController {
+	
+	
 	@RequestMapping("/comments")
 	public String loginForm() {
 		return "comments/commentsForm";
 	}
+	 
 }
