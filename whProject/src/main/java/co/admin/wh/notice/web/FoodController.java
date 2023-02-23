6a11712d06f38d2e182edb3d8ec1f4be951ee62e
gim-eunjin <gim-eunjin@localhost>
2023-02-23 11:34:16 +0900

    Merge branch 'main' of https://github.com/deokjinkkkkk/WH.git into eunjin

--- whProject/src/main/java/co/admin/wh/notice/web/FoodController.java
+++ whProject/src/main/java/co/admin/wh/notice/web/FoodController.java
@@ -3,21 +3,33 @@ package co.admin.wh.notice.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import co.admin.wh.notice.mapper.FoodMapper;
 import co.admin.wh.notice.service.FoodService;
+import co.admin.wh.notice.vo.PagingVO;
 
 @Controller
-
 public class FoodController {
 	
 	@Autowired FoodService foodService;
+	@Autowired 
+	private FoodMapper foodMapper;
 	
-	@Autowired FoodMapper foodMapper;
 	
 	@RequestMapping("/food")
-	public String foodForm(Model model) {
+	public String foodList(Model model) {
 		model.addAttribute("foodList", foodMapper.getFoodList()); 
 		return "notice/foodList";
 	}
+	
+	@RequestMapping("/foodForm")
+	public String foodForm(Model model) {
+		return "notice/foodForm";
+	}
+	
+
+	
 }
