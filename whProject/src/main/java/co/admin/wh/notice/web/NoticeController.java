27521252491a7579c0f405bc1bf629691a643fcd
gkals0230@naver.com <gkals0230@naver.com>
2023-02-24 17:41:05 +0900

    23.02.24 학원

--- whProject/src/main/java/co/admin/wh/notice/web/NoticeController.java
+++ whProject/src/main/java/co/admin/wh/notice/web/NoticeController.java
@@ -3,14 +3,15 @@ package co.admin.wh.notice.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import co.admin.wh.notice.mapper.NoticeMapper;
 import co.admin.wh.notice.service.NoticeService;
+import co.admin.wh.notice.vo.NoticeSearchVO;
 import co.admin.wh.notice.vo.NoticeVO;
+import co.admin.wh.notice.vo.Paging;
 
 @Controller
 public class NoticeController {
@@ -19,8 +20,15 @@ public class NoticeController {
 	@Autowired NoticeMapper noticeMapper;
 	
 	@RequestMapping("/notice")
-	public String notice(Model model) {
-		model.addAttribute("noticelists", noticeService.getNoticeList()); 
+	public String notice(Model model, @ModelAttribute("svo")NoticeSearchVO svo,  Paging paging) {
+		paging.setPageUnit(5);//한 페이지에 풀력할 레코드 건수
+		paging.setPageSize(10); //한 페이지에 보여질 페이지 갯수
+		
+		paging.setTotalRecord(noticeService.getCountTotal(svo));
+		svo.setFirst(paging.getFirst());
+		svo.setLast(paging.getLast());
+		
+		model.addAttribute("noticelists", noticeService.getNoticeList(svo)); 
 		return "notice/noticelist";
 	}
 	
@@ -30,16 +38,18 @@ public class NoticeController {
 	}
 	
 	@RequestMapping("/noticeInsert.do")
-	public String noticeInsert(NoticeVO vo, Model model) {
-		model.addAttribute("noticelists", noticeService.getNoticeList());
+	public String noticeInsert(NoticeVO vo, Model model, NoticeSearchVO svo ) {
+		model.addAttribute("noticelists", noticeService.getNoticeList(svo));
 		noticeMapper.noticeInsert(vo);
 		return "redirect:notice";
 	}
 	
-	@RequestMapping("/noticDetile")
-	public String noticeDe(NoticeVO svo, Model model) {
-		model.addAttribute("noticelists", noticeService.ndetil(svo));
-		return "notice/noticDetile";
+	@GetMapping("/noticeDetail/{noticeCode}")
+	public String noticeDe(NoticeVO vo, Model model) {
+		model.addAttribute("noticelists", noticeService.noticendetil(vo));
+		return "notice/noticeDetail";
 	}
+	
+	
 	
 }
