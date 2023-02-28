9b6bd0301971703bbb69ff97b3f63cc746c57d9c
seonhee <ads707@naver.com>
2023-02-28 17:54:57 +0900

    2023_02_28 페이징 시도

--- whProject/src/main/java/co/admin/wh/trip/mapper/TripMapper.java
+++ whProject/src/main/java/co/admin/wh/trip/mapper/TripMapper.java
@@ -2,10 +2,16 @@ package co.admin.wh.trip.mapper;
 
 import java.util.List;
 
+import co.admin.wh.trip.vo.TripSearchVO;
 import co.admin.wh.trip.vo.TripVO;
 
 public interface TripMapper {
 	
-	void insertInfo(TripVO tripVO);
-	List<TripVO> tripList();
+	void insertInfo(TripVO tripVO); // api DB 저장
+	
+	List<TripVO> tripList(); // 관광지 전체 리스트
+	
+	int getTripTotla(TripSearchVO svo); // 페이징(총게시글)
+	
+	
 }
