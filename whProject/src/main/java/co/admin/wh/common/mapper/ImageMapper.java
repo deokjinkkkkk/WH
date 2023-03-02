658fa3e2c44a0a44dc59768871c6a511c1df47e9
gkals0230@naver.com <gkals0230@naver.com>
2023-03-02 17:36:09 +0900

    2023.03.02학원에서

--- whProject/src/main/java/co/admin/wh/common/mapper/ImageMapper.java
+++ whProject/src/main/java/co/admin/wh/common/mapper/ImageMapper.java
@@ -1,8 +1,11 @@
 package co.admin.wh.common.mapper;
 
+
+
 import co.admin.wh.common.vo.ImageVO;
 
 public interface ImageMapper {
 	
 	String imageSelect(ImageVO ivo);
+	String imgNotice(ImageVO ivo);
 }
