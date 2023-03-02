4b7bf96db7a8c8b253213e8eb09fa74cdb7c7fc8
deokjinkkkkk <antan3957@naver.com>
2023-03-02 09:45:01 +0900

    414124

--- whProject/src/main/java/co/admin/wh/config/WebSecirityConfig.java
+++ whProject/src/main/java/co/admin/wh/config/WebSecirityConfig.java
@@ -1,11 +1,9 @@
 package co.admin.wh.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,8 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import co.admin.wh.member.vo.MemberVO;
 
 @Configuration
 @EnableWebSecurity
@@ -34,7 +30,9 @@ public class WebSecirityConfig{
 				.passwordParameter("password")
 				.permitAll()
 			)
-			.logout((logout) -> logout.permitAll());
+			.logout((logout) -> logout.permitAll())
+			.csrf().disable();
+		
 
 		return http.build();
 	}
