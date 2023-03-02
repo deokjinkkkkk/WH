1865a5046d07130cfafbea4051503269af6a8971
jinjin777777 <zzz5860@naver.com>
2023-03-02 14:54:19 +0900

    230302 jin afternoon

---
+++ whProject/src/main/java/co/admin/wh/common/service/ImageService.java
@@ -0,0 +1,14 @@
+package co.admin.wh.common.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import co.admin.wh.common.vo.ImageVO;
+
+public interface ImageService {
+	
+	String imageSelect(ImageVO ivo);
+
+	String saveImage(MultipartFile[] file, String saveFolder);
+	
+	
+}
