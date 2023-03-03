e304b2439154fe780b243e8a4404b17b96952cd9
gkals0230@naver.com <gkals0230@naver.com>
2023-03-03 17:42:59 +0900

    2023.03.03

--- whProject/src/main/java/co/admin/wh/common/service/GreatSurviceImpl.java
+++ whProject/src/main/java/co/admin/wh/common/service/GreatSurviceImpl.java
@@ -1,28 +1,47 @@
 package co.admin.wh.common.service;
 
-import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import co.admin.wh.common.mapper.GreatMapper;
 import co.admin.wh.common.vo.GreatVO;
+
 @Service
 public class GreatSurviceImpl implements GreatService {
 	@Autowired GreatMapper mapper;
-	
+
 	@Override
-	public List<Map<String, Object>> myGreatList() {
-		//마이페이지 좋아요 리스트 출력 
-		return mapper.myGreatList();
+	public int greatCheck(int greatNcode, String id) {
+		return mapper.greatCheck(greatNcode, id);
 	}
 
 	@Override
-	public int greatDelete(GreatVO vo) {
-		//마이페이지 좋아요 삭제 
-		return mapper.greatDelete(vo);
+	public int greatUP(GreatVO vo) {
+		return mapper.greatUP(vo);
 	}
+
+	@Override
+	public int greatDown(GreatVO vo) {
+		return mapper.greatDown(vo);
+	}
+
+	@Override
+	public int greatTotal(int greatNcode) {
+		return greatTotal(greatNcode);
+	}
+
+	
+	
+
+	
+
+	
+
+	
+	
+	
+
 
 
 }
