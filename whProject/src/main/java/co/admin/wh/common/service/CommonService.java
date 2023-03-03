395c0c96cafbc1d114ede1e6967a6efac5634c53
jinjin777777 <zzz5860@naver.com>
2023-03-03 16:12:42 +0900

    230303 jin afternoon

---
+++ whProject/src/main/java/co/admin/wh/common/service/CommonService.java
@@ -0,0 +1,12 @@
+package co.admin.wh.common.service;
+
+import java.util.List;
+
+import co.admin.wh.common.vo.CommonVO;
+
+public interface CommonService {
+	
+	List<CommonVO> commonLocal();
+	
+	List<CommonVO> commonGroup();
+}
