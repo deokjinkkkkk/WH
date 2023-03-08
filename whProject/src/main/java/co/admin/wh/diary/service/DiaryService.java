6f3dece71826cba8a30bb875947a20fafc1d895a
gkals0230@naver.com <gkals0230@naver.com>
2023-03-08 17:40:52 +0900

    3.8.학원입댜아아

--- whProject/src/main/java/co/admin/wh/diary/service/DiaryService.java
+++ whProject/src/main/java/co/admin/wh/diary/service/DiaryService.java
@@ -3,12 +3,11 @@ package co.admin.wh.diary.service;
 import java.util.List;
 
 import co.admin.wh.common.vo.ImageVO;
-import co.admin.wh.diary.vo.DiarySearchVO;
 import co.admin.wh.diary.vo.DiaryVO;
 
 public interface DiaryService {
 	
-	List<DiaryVO> getDiaryList(DiarySearchVO svo); //리스트 + 페이징 
+	List<DiaryVO> getDiaryList(); //리스트 
 	
 	int diaryInsert(DiaryVO vo); //등록 
 	
@@ -18,7 +17,6 @@ public interface DiaryService {
 	
 	int diaryDelete(DiaryVO vo); //삭제 
 	
-	int getCountTotal(DiarySearchVO svo); //페이징 
 	
 	int imgInsert(ImageVO ivo); //이미지 테이 등록 
 	
