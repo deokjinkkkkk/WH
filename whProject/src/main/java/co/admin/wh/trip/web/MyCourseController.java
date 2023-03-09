9fe228809e24bff24002085c09e13009dcdeda43
Pluto <Pluto@DESKTOP-IBCCQMG>
2023-03-10 01:14:18 +0900

    230310_ 나만의코스 제목 등록 후 바로 리스트 출력 해보기...

--- whProject/src/main/java/co/admin/wh/trip/web/MyCourseController.java
+++ whProject/src/main/java/co/admin/wh/trip/web/MyCourseController.java
@@ -3,15 +3,21 @@ package co.admin.wh.trip.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import co.admin.wh.trip.service.MyCourseService;
 import co.admin.wh.trip.vo.MyCourseFreeVO;
 import co.admin.wh.trip.vo.MyCourseVO;
+import co.admin.wh.trip.mapper.MyCourseMapper;
 import co.admin.wh.trip.service.MyCourseFreeService;
 
 @Controller
 public class MyCourseController {
+	
+	@Autowired MyCourseMapper myCourseMapper;
 	
 	@Autowired
 	MyCourseService myCourseService;
@@ -26,9 +32,21 @@ public class MyCourseController {
 		return "trip/myCourseList";
 	}
 	
+	// 제목 입력하기
+	@PostMapping("/myCourseInsert")
+	@ResponseBody
+	public String myCourseInsert(@RequestBody MyCourseVO vo) {
+		int result = myCourseMapper.titleInsert(vo);
+		System.out.println(result);
+		String resultValue = "fail";
+		if(result > 0) {
+			resultValue = "success";			
+		}
+		return resultValue;
+	}
+	
 	@RequestMapping("/myCourseDetail")
 	public String CourseDetail(Model model) {
-		model.addAttribute("test1", myCourseFreeService.myCourseTripList());
 		return "trip/myCourseDetail";
 	}
 }
