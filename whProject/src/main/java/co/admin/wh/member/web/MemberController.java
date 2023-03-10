91c3ff13fd4d2b6c41f450a0bc3f9b582940bcc6
deokjinkkkkk <antan3957@naver.com>
2023-03-10 18:43:12 +0900

    관리자 회원관리

--- whProject/src/main/java/co/admin/wh/member/web/MemberController.java
+++ whProject/src/main/java/co/admin/wh/member/web/MemberController.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -91,14 +92,21 @@ public class MemberController {
 
 	@PostMapping("/memberUpdate")
 	public String memberUpdate(MemberVO vo) {
-		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		vo.setPass(passwordEncoder.encode(vo.getPass()));
+		if(vo.getPass() != null) {
+			BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+			vo.setPass(passwordEncoder.encode(vo.getPass()));
+			memberMapper.memberUpdate(vo);
+			return "content/main";
+		}
+		System.out.println(vo.getEmail()+"++++++++++++++++++++++++++++++++");
 		memberMapper.memberUpdate(vo);
-		return "content/main";
+		return "redirect:/admemList";
 	}
 
 	@PostMapping("/memberDelete")
 	public String memberDelete(MemberVO vo) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		vo.setPass(passwordEncoder.encode(vo.getPass()));
 		int Del = memberMapper.memDel(vo);
 		if (Del == 1) {
 			memberMapper.memberDelete(vo);
@@ -109,78 +117,29 @@ public class MemberController {
 		return "content/main";
 	}
 
-	/*
-	 * @GetMapping("/login/kakao") public String kakaoLogin(String code) {
-	 * userService.kakaoLogin(code); return "redirect:/"; }
-	 */
 	@RequestMapping("/admemList")
-	public String adMemList(MemberVO vo) {
-		memberMapper.adMemberList();
+	public String adMemList(MemberVO vo, Model model) {
+		model.addAttribute("mem",memberMapper.adMemberList());
 		return "admin/memberAdmin";
 	}
-
-//	@RequestMapping("/login/kakao")
-//	@ResponseBody
-//	public String kakaoLogin(String code) {
-//		String contentType = "application/x-www-form-urlencoded;charset=utf-8";
-//		String restApi = "eca6df115c265db993762594751419b6";
-//		String uri = "http://localhost/login/kakao";
-//		RestTemplate resttemp = new RestTemplate();
-//
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.add("Content-type", contentType);
-//
-//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-//		params.add("grant_type", "authorization_code");
-//		params.add("client_id", restApi);
-//		params.add("redirect_uri", uri);
-//		params.add("code", code);
-//		HttpEntity<MultiValueMap<String, String>> kakaoToken = new HttpEntity<>(params, headers);
-//
-//		ResponseEntity<String> response = resttemp.exchange(
-//				"https://kauth.kakao.com/oauth/token", 
-//				HttpMethod.POST,
-//				kakaoToken,
-//				String.class);
-//
-//		// 받아온 토큰 정보를 객체로 저장
-//		ObjectMapper objectMapper = new ObjectMapper();
-//		KakaoOAuthToken oauToken = null;
-//		try {
-//			oauToken = objectMapper.readValue(response.getBody(), KakaoOAuthToken.class);
-//		} catch (JsonProcessingException e) {
-//			e.printStackTrace();
-//		}
-//		//토큰 정보를받아 프로필 요청하기
-//		RestTemplate resttempPr = new RestTemplate();
-//
-//		HttpHeaders headersPr = new HttpHeaders();
-//		headersPr.add("Authorication", "Bearer"+oauToken.getAccessToken());
-//		headersPr.add("Content-type", contentType);
-//		
-//		HttpEntity<MultiValueMap<String, String>> kakaoProfile = new HttpEntity<>(headersPr);
-//		
-//		//사용자 정보 요청
-//		ResponseEntity<String> responsePr = resttempPr.exchange(
-//				"https://kauth.kakao.com/v2/user/me", 
-//				HttpMethod.POST,
-//				kakaoProfile,
-//				String.class);
-//		ObjectMapper objectMapper2 = new ObjectMapper();
-//		KakaoProfile kakaoPro = null;
-//		try {
-//			kakaoPro = objectMapper2.readValue(responsePr.getBody(), KakaoProfile.class);
-//		} catch (JsonProcessingException e) {
-//			e.printStackTrace();
-//		}
-//		
-//		User kakaoUser = User.builder()
-//				.username(kakaoPro.getKakao_account().getEmail()+ "_" + kakaoPro.getId())
-//				.password("1234")
-
-//				.build();
-//				
-//		
-//		return "카카오" + response;
-//	}
+	
+	@RequestMapping("/passChk")
+	@ResponseBody
+	public String passCheck(MemberVO vo,boolean n) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		vo.setPass(passwordEncoder.encode(vo.getPass()));
+		n = memberMapper.passChk(vo.getId(),vo.getPass());
+		if (n) {
+			return "success";
+		} else {
+			return "fali";
+		}
+	}
+	
+	@PostMapping("/adminDelete")
+	public String adminDelete(MemberVO vo) {
+		memberMapper.memDel(vo);
+		memberMapper.memberDelete(vo);
+		return "redirect:/admemList";
+	}
 }
