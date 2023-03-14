c1191ea315e6e3d051e052ae1481dccb26bae0ee
Pluto <Pluto@DESKTOP-IBCCQMG>
2023-03-15 01:27:40 +0900

    230315_ home

--- whProject/src/main/java/co/admin/wh/trip/vo/MyCourseFreeVO.java
+++ whProject/src/main/java/co/admin/wh/trip/vo/MyCourseFreeVO.java
@@ -7,7 +7,7 @@ public class MyCourseFreeVO {
 
 	// 나만의 코스 상세페이지
 	int tripUniqueNumber; // 여행코드
-	String myCourseCode; // 나만의코스코드...
+	int myCourseCode; // 나만의코스코드...
 	String couOrd; // 순서
 	int myCouFreeCode; // 나만의코스지역번호
 	
--- whProject/src/main/java/co/admin/wh/trip/vo/MyCourseVO.java
+++ whProject/src/main/java/co/admin/wh/trip/vo/MyCourseVO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class MyCourseVO {
 	//나만의 코스 -> 코스 목록, 리스트 화면
-	String myCourseCode; //나만의코스 번호
+	int myCourseCode; //나만의코스 번호
 	String theme; //코스테마
 	String myCouDate; //코스일정
 	String myCouName; //코스명
